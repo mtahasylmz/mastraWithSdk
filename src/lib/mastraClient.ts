69f7b1fd34598c7c2daa4833f734358f98c6d0da
MTaha <mtahasylmz@MTahas-MacBook-Pro.local>
2025-07-19 23:45:42 +0300

    removedMastraFolder

--- src/lib/mastraClient.ts
+++ src/lib/mastraClient.ts
@@ -1,6 +1,6 @@
 import { MastraClient } from "@mastra/client-js";
  
 export const mastra_sdk = new MastraClient({
-    baseUrl: process.env.NEXT_PUBLIC_MASTRA_API || "http://localhost:4111",
+    baseUrl: process.env.NEXT_PUBLIC_MASTRA_API!,
     retries: 3,
   });
